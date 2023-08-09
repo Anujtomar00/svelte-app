7e8c381ce7abd971e0bd06ac73265fbfffd840a1
Anuj Tomar <anuj.tomar@knoldus.com>
2023-08-09 19:43:17 +0530

    Added Dashboard

--- src/routes/(authenticated)/batches_table/svelteTable.svelte
+++ src/routes/(authenticated)/batches_table/svelteTable.svelte
@@ -288,25 +288,46 @@ function filterData() {
     font-family: Arial, sans-serif;
     font-size: 14px;
     margin-bottom: 20px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    background-color: #fff; /* Set a background color for the table */
+  }
+
+  .data-table {
+    width: 100%;
+    border-collapse: collapse;
+    font-family: Arial, sans-serif;
+    font-size: 14px;
+    margin-bottom: 20px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    background-color: #fff; /* Set a background color for the table */
   }
 
   .data-table thead th {
     background-color: #f2f2f2;
     color: #333333;
-    text-align: left;
-    padding: 10px;
+    text-align: center; /* Center the header text */
+    padding: 12px; /* Increase padding for better spacing */
     font-weight: bold;
     border-bottom: 1px solid #cccccc;
   }
 
-  .data-table td {
-    padding: 10px;
+  .data-table tbody td {
+    padding: 12px; /* Increase padding for better spacing */
     border-bottom: 1px solid #cccccc;
+    text-align: center; /* Center the cell content */
+    vertical-align: middle; /* Center cell content vertically */
   }
 
-  .data-table td.align-right {
-    text-align: right;
+  .data-table tbody tr:nth-child(even) {
+    /* Apply alternating row colors */
+    background-color: #f8f8f8;
   }
+
+  .data-table tbody tr:hover {
+    /* Apply hover effect */
+    background-color: #f0f0f0;
+  }
+
 
   .notification-span {
     width: 19rem;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,54 @@
 <script lang="ts">
-	import { session } from "$lib/stores/session";
+  import { session } from "$lib/stores/session";
+  import Analytics from "./Analytics.svelte";
 </script>
 
-<div class="prose lg:prose-lg" style="margin-left:7rem;">
-	<h1>
-		Learning & Development Management
-	</h1>
-	<p>This project shows you how to manage the progress of Interns.</p>
-	<p>
-		In this project you can login to the system and can see the scores of
-		interns with the help of charts and their score.
-	</p>
-	<p>
-		Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, vero
-		dolor! Deleniti minus, vel quis omnis rem tempore tenetur dignissimos ullam
-		debitis veniam aliquid molestiae labore sit vero! Deleniti, deserunt.
-	</p>
-	<p class="flex items-center gap-4 mt-12">
-		{#if $session?.user}
-			<a href="/dashboard" class="btn btn-primary">Dashboard</a>
-			<a href="/settings" class="btn" style="color:white;">Settings</a>
-		{:else}
-			<a href="/signup" class="btn btn-primary">Sign Up</a>
-			<a href="/login" class="btn" style="color:white;">Log In</a>
-		{/if}
-	</p>
-</div>
+{#if $session?.user}
+  <div class="prose custom-prose-container p-4">
+    <h1>Learning & Development Management</h1>
+    <div class="dashboard">
+      <Analytics />
+		<!-- <div class="dashboard-button">
+      		<a href="/dashboard" class="btn btn-primary">Dashboard</a>
+      		<a href="/settings" class="btn" style="color:white;  margin-left:1rem;">Settings</a>
+		</div> -->
+    </div>
+  </div>
+{:else}
+  <div class="prose lg:prose-lg" style="margin-left:7rem;">
+    <h1>Learning & Development Management</h1>
+    <p>This project shows you how to manage the progress of Interns.</p>
+    <p>
+      In this project you can login to the system and can see the scores of
+      interns with the help of charts and their score.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, vero
+      dolor! Deleniti minus, vel quis omnis rem tempore tenetur dignissimos
+      ullam debitis veniam aliquid molestiae labore sit vero! Deleniti,
+      deserunt.
+    </p>
+    <p class="flex items-center gap-4 mt-12">
+      <a href="/signup" class="btn btn-primary">Sign Up</a>
+      <a href="/login" class="btn" style="color:white;">Log In</a>
+    </p>
+  </div>
+{/if}
+
+
+<style>
+	 .custom-prose-container {
+    max-width: 100%; /* Adjust the width as per your preference */
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .dashboard{
+	display: flex;
+	flex-direction: column;
+  }
+
+  .dashboard-button{
+	display: flex;
+  }
+</style>
