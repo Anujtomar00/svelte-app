dcb89cad77dcbee372f591c99970beeeb7dbd34b
Aditya <adityagangwar516@gmail.com>
2024-10-18 13:59:27 +0530

    Fixed Menu

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -11,8 +11,30 @@
 		faHome
 	} from "@fortawesome/free-solid-svg-icons";
 	import Fa from "svelte-fa";
+	import { onMount } from 'svelte';
 	import "../app.postcss";
 	import type { PageData } from "./$types";
+
+	let isMenuOpen = false;
+	let menuRef: HTMLElement;
+
+	function toggleMenu(event: MouseEvent) {
+		event.stopPropagation();
+		isMenuOpen = !isMenuOpen;
+	}
+
+	function handleClickOutside(event: MouseEvent) {
+		if (menuRef && !menuRef.contains(event.target as Node) && isMenuOpen) {
+			isMenuOpen = false;
+		}
+	}
+
+	onMount(() => {
+		document.addEventListener('click', handleClickOutside);
+		return () => {
+			document.removeEventListener('click', handleClickOutside);
+		};
+	});
 
 	$: menu_items = $session?.user
 		? [
@@ -61,13 +83,20 @@
 				</a>
 			</div>
 			<div class="flex items-center">
-				<nav class="relative group">
-					<button class="btn btn-ghost gap-2" aria-haspopup="true" aria-expanded="false">
+				<nav class="relative " bind:this={menuRef}>
+					<button
+						class="btn btn-ghost gap-2"
+						aria-haspopup="true"
+						aria-expanded={isMenuOpen}
+						on:click={toggleMenu}
+					>
 						<Fa icon={faBars} />
 						<span class="hidden sm:inline">Menu</span>
 					</button>
-					<ul style="list-style-type: none;"
-						class="absolute right-0 mt-2 w-48 bg-base-200 rounded-md shadow-lg py-1 z-10 hidden group-hover:block"
+					<ul
+					     style="list-style-type: none;"
+						class="absolute right-0 mt-2 w-48 bg-base-200 rounded-md shadow-lg py-1 z-10"
+						class:hidden={!isMenuOpen}
 						role="menu"
 						aria-orientation="vertical"
 						aria-labelledby="user-menu"
@@ -77,8 +106,9 @@
 								<a
 									href={item.href}
 									data-sveltekit-reload={item.reload ? "" : "off"}
-									class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+									class="flex no-underline items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
 									role="menuitem"
+									on:click={() => isMenuOpen = false}
 								>
 									<Fa icon={item.icon} class="mr-2" />
 									<span>{item.label}</span>
@@ -91,12 +121,3 @@
 		</div>
 	</div>
 </header>
-
-<style>
-	nav:hover ul, nav ul:hover {
-		display: block;
-	}
-	.group:hover .group-hover\:block {
-		display: block;
-	}
-</style>
